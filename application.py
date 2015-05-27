4dab5a210fbefb27bc804eb8eb33149c6753fac7
Greg Palen <codingvirtual@gmail.com>
2015-05-27 14:49:42 -0500

    add new item to existing category create style "protected"

--- application.py
+++ application.py
@@ -34,7 +34,7 @@ def category_list():
 # CATEGORY:         Add
 # REQUIRED PARAMS:  none
 # PERMISSIONS:      Logged-in user
-@app.route('/new_category.html', methods = ['GET','POST'])
+@app.route('/category/add', methods = ['GET', 'POST'])
 def addCategory():
     if request.method == 'POST':
         newCategory = Category(name = request.form['name'])
@@ -49,7 +49,7 @@ def addCategory():
 # REQUIRED PARAMS:  Category ID to retrieve current values (GET)
 #                   Form data with update info (POST)
 # PERMISSIONS:      Logged-in user
-@app.route('/category/<int:category_id>/edit', methods=['GET','POST'])
+@app.route('/category/<int:category_id>/edit', methods=['GET', 'POST'])
 
 
 # CATEGORY:         Delete
@@ -69,16 +69,34 @@ def addCategory():
 # PERMISSIONS:      Public
 @app.route('/category/<int:category_id>/items')
 def itemList(category_id):
-    category = session.query(Category).filter_by(id = category_id).one()
-    categoryitems = session.query(CategoryItem).filter_by(category_id = category_id).all()
-    return render_template('items_in_category.html', categoryname=category.name, categoryitems=categoryitems)
+    category = session.query(Category).filter_by(id=category_id).one()
+    categoryitems = session.query(CategoryItem)\
+        .filter_by(category_id = category_id).all()
+    return render_template('items_in_category.html',
+                           category_id=category_id,
+                           categoryname=category.name,
+                           categoryitems=categoryitems)
 
 
 # ITEMS:            Add
 # REQUIRED PARAMS:  Form data with item info
 # PERMISSIONS:      Logged-in user
-@app.route('/item/<int:category_id>/add', methods=['POST'])
-
+@app.route('/item/<int:category_id>/add', methods=['GET', 'POST'])
+def addItem(category_id):
+    if request.method == 'POST':
+        category = session.query(Category).filter_by(id=category_id).one()
+        new_item=CategoryItem(title=request.form['title'],
+                              description=request.form['description'],
+                              category=category)
+        session.add(new_item)
+        session.commit()
+        return redirect(url_for('itemList', category_id=category_id))
+    else:
+        category = session.query(Category).filter_by(id=category_id).one()
+        return render_template(
+            'new_category_item.html',
+            category_id=category_id,
+            category_name=category.name)
 
 # ITEMS: Edit
 # REQUIRED PARAMS:  Item ID to display current item info in form (GET)
@@ -100,7 +118,8 @@ def itemList(category_id):
 @app.route('/items/<int:category_id>/JSON')
 def itemlistJSON(category_id):
     category = session.query(Category).filter_by(id = category_id).one()
-    categoryitems = session.query(CategoryItem).filter_by(category_id = category_id).all()
+    categoryitems = session.query(CategoryItem)\
+        .filter_by(category_id = category_id).all()
     return jsonify(CategoryItems = [i.serialize for i in categoryitems])
 
 
